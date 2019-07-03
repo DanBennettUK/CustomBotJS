60763f6d19fb9cbdaa9414228e9b28c89f667700
Dan Bennett <dan@danbennett.me>
2019-07-03 16:26:55 +0100

    :construction: Started basics of a $help command

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Require discord.js module
 const Discord = require('discord.js');
 
+// Other Requirements from module
+const { Client, RichEmbed } = require('discord.js');
+
 // Create client
 const client = new Discord.Client();
 
@@ -21,7 +24,6 @@ client.on(`debug`, (e) => console.info(e));
 client.login(config.token);
 
 // Start parsing messages
-
 client.on("message", async message => {
   // Ignore bots own messages.
   if (message.author.bot) return;
@@ -36,10 +38,39 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  // Help command
+  if (command === `help`) {
+    if (!message.member.roles.some(role => role.id === config.host_role_id)) {
+      const helpEmbed = new Discord.RichEmbed()
+        .setColor('ffffff')
+        .setAuthor(client.user.username, client.user.displayAvatarURL)
+        .setDescription(`**General Commands for users**
+    \`${config.prefix}help\` - lists all commands
+    \`${config.prefix}ping\` - checks Discord API response`)
+        .setTimestamp()
+        .setFooter(`Version: ${config.version}`);
+      message.channel.send(helpEmbed);
+    } else if (message.member.roles.some(role => role.id === config.host_role_id)) {
+      const hostHelpEmbed = new Discord.RichEmbed()
+        .setColor('ffffff')
+        .setAuthor(client.user.username, client.user.displayAvatarURL)
+        .setDescription(`**General Commands for hosts**
+        \`${config.prefix}help\` - lists all commands
+        \`${config.prefix}ping\` - checks Discord API response`)
+        .setTimestamp()
+        .setFooter(`Version: ${config.version}`);
+      message.channel.send(hostHelpEmbed);
+    }
+  }
+
+  // Ping command
   if (command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const ping_message = await message.channel.send("Ping?");
     ping_message.edit(`Pong! Latency is ${ping_message.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
+
+
+
 });
