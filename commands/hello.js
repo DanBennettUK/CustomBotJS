006d0e7e71273431da9f6a3b8aa81e54c6b5bf03
Dan Bennett <dan@danbennett.me>
2019-07-06 15:09:12 +0100

    :poop: Testing role mentions

--- commands/hello.js
+++ commands/hello.js
@@ -1,4 +1,8 @@
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
+	const customRole = message.guild.roles.find(
+		findRole => findRole.id === client.config.custom_role_id
+	);
+
 	if (message.channel.id === client.config.host_channel_id) {
 		message.delete();
 		const channel = client.channels.get(client.config.host_channel_id);
@@ -9,6 +13,6 @@ exports.run = async (client, message, args) => {
 	if (message.channel.id === client.config.games_channel_id) {
 		message.delete();
 		const channel = client.channels.get(client.config.games_channel_id);
-		channel.send('Hello you! :wave:');
+		channel.send('Hello ' + customRole + ':wave:');
 	}
 };
