e4e4d27e5d22eb06b0b4e191bfed3ded25ff678e
Dan Bennett <dan@danbennett.me>
2019-07-06 16:59:39 +0100

    :poop: Tested setting role to mentionable

--- commands/hello.js
+++ commands/hello.js
@@ -12,7 +12,16 @@ exports.run = async (client, message) => {
 	// Send message to games channel
 	if (message.channel.id === client.config.games_channel_id) {
 		message.delete();
+		customRole
+			.setMentionable(true, 'Role needs to be pinged')
+			.catch(console.error);
 		const channel = client.channels.get(client.config.games_channel_id);
 		channel.send('Hello ' + customRole + ':wave:');
+
+		setTimeout(function() {
+			customRole
+				.setMentionable(false, 'Role no longer needs to be pinged')
+				.catch(console.error);
+		}, 10000);
 	}
 };
