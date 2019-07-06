19262c7cde36e9d9cd31415c9f4f7f38dbbcb59c
Dan Bennett <dan@danbennett.me>
2019-07-06 17:03:28 +0100

    :bug: Fixed timer calculation

--- commands/password.js
+++ commands/password.js
@@ -146,7 +146,7 @@ exports.run = async (client, message, args) => {
 					if (client.config.host_channel_messages === true) {
 						host_channel.send('Password has been posted!');
 					}
-				}, timer * 60 * 100);
+				}, timer * 60 * 1000);
 			});
 	}
 	catch (error) {
