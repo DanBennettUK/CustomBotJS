26ac1f9162a91163087ca13083fcc5225fefca3f
Dan Bennett <dan@danbennett.me>
2019-07-07 11:18:42 +0100

    :art: Removing duplicate code (fixes  #4)

--- commands/squadvote.js
+++ commands/squadvote.js
@@ -49,9 +49,8 @@ exports.run = async (client, message, args) => {
         },
     };
 
-    if (message_squad_sizes.length == 0) {
-        // If the array is empty, only vote for 1, 2, 4 or 8.
-        const squad_sizes = ['1', '2', '4', '8'];
+    // Function to send the squad vote messages
+    async function squadTryMessage(squad_sizes) {
         try {
             await games_channel
                 .send({ embed: squadVoteMessage })
@@ -144,100 +143,16 @@ exports.run = async (client, message, args) => {
             console.log(`${error}`);
         }
     }
+
+    if (message_squad_sizes.length == 0) {
+        // If the array is empty, only vote for 1, 2, 4 or 8.
+        const squad_sizes = ['1', '2', '4', '8'];
+        squadTryMessage(squad_sizes);
+    }
     else if (message_squad_sizes[0] == 'all') {
         // If the array is 'all' - post up to 10
         const squad_sizes = squads_range;
-        try {
-            await games_channel
-                .send({ embed: squadVoteMessage })
-                .then(async embedMessage => {
-                    // Checks if message is deleted
-                    const checkIfDeleted = setInterval(function() {
-                        if (embedMessage.deleted) {
-                            clearTimeout(timeToVote);
-                            clearInterval(checkIfDeleted);
-                        }
-                    }, 1000);
-
-                    for (let i = 0; i < squad_sizes.length; i++) {
-                        await embedMessage.react(
-                            emojiCharacters[squad_sizes[i]]
-                        );
-                    }
-                    if (client.config.custom_role_ping == true) {
-                        await customRole
-                            .setMentionable(true, 'Role needs to be pinged')
-                            .catch(console.error);
-                        await games_channel
-                            .send(customRole + ' - get voting!')
-                            .then(msg =>
-                                setTimeout(function() {
-                                    msg.delete();
-                                }, client.config.default_timer * 60 * 1000)
-                            )
-                            .catch(console.error);
-                        await customRole
-                            .setMentionable(
-                                false,
-                                'Role no longer needs to be pinged'
-                            )
-                            .catch(console.error);
-                    }
-                    const timeToVote = setTimeout(function() {
-                        const reactions = embedMessage.reactions.array();
-                        let reactionID;
-                        let maxCount = 0;
-                        for (let i = 0; i < reactions.length; i++) {
-                            if (reactions[i].count > maxCount) {
-                                maxCount = reactions[i].count;
-                                reactionID = i;
-                            }
-                        }
-
-                        const draws = [];
-                        for (let i = 0, j = 0; i < reactions.length; i++) {
-                            if (reactions[i].count == maxCount) {
-                                draws[j] = i;
-                                j++;
-                            }
-                        }
-                        if (draws.length > 1) {
-                            reactionID =
-                                draws[
-                                    Math.floor(
-                                        Math.random() * Math.floor(draws.length)
-                                    )
-                                ];
-                        }
-
-                        const squadResult = {
-                            color: 0x009900,
-                            title: `${title}`,
-                            fields: [
-                                {
-                                    name: `${winText}`,
-                                    value: `${reactions[reactionID]._emoji}`,
-                                },
-                            ],
-                            timestamp: new Date(),
-                            footer: {
-                                icon_url: client.user.avatarURL,
-                            },
-                        };
-
-                        embedMessage.delete();
-                        games_channel.send({ embed: squadResult });
-                        if (client.config.host_channel_messages === true) {
-                            host_channel.send(
-                                `${winText} ${reactions[reactionID]._emoji}`
-                            );
-                        }
-                    }, client.config.default_timer * 60 * 1000);
-                });
-        }
-        catch (error) {
-            console.log(`${error}`);
-        }
+        squadTryMessage(squad_sizes);
     }
     else if (isNaN(message_squad_sizes[0])) {
         // If it's not a number...
@@ -248,111 +163,16 @@ exports.run = async (client, message, args) => {
     else {
         // Picked squad sizes by host
         // Check the array fits in the range we want
-
         const squads_correct_range = containsAny(
             squads_range,
             message_squad_sizes
         );
-
         if (squads_correct_range === false) {
             host_channel.send('A number is out of range');
         }
         else {
             const squad_sizes = message_squad_sizes;
-            try {
-                await games_channel
-                    .send({ embed: squadVoteMessage })
-                    .then(async embedMessage => {
-                        // Checks if message is deleted
-                        const checkIfDeleted = setInterval(function() {
-                            if (embedMessage.deleted) {
-                                clearTimeout(timeToVote);
-                                clearInterval(checkIfDeleted);
-                            }
-                        }, 1000);
-
-                        for (let i = 0; i < squad_sizes.length; i++) {
-                            await embedMessage.react(
-                                emojiCharacters[squad_sizes[i]]
-                            );
-                        }
-                        if (client.config.custom_role_ping == true) {
-                            await customRole
-                                .setMentionable(true, 'Role needs to be pinged')
-                                .catch(console.error);
-                            await games_channel
-                                .send(customRole + ' - get voting!')
-                                .then(msg =>
-                                    setTimeout(function() {
-                                        msg.delete();
-                                    }, client.config.default_timer * 60 * 1000)
-                                )
-                                .catch(console.error);
-                            await customRole
-                                .setMentionable(
-                                    false,
-                                    'Role no longer needs to be pinged'
-                                )
-                                .catch(console.error);
-                        }
-                        const timeToVote = setTimeout(function() {
-                            const reactions = embedMessage.reactions.array();
-                            let reactionID;
-                            let maxCount = 0;
-                            for (let i = 0; i < reactions.length; i++) {
-                                if (reactions[i].count > maxCount) {
-                                    maxCount = reactions[i].count;
-                                    reactionID = i;
-                                }
-                            }
-
-                            const draws = [];
-                            for (let i = 0, j = 0; i < reactions.length; i++) {
-                                if (reactions[i].count == maxCount) {
-                                    draws[j] = i;
-                                    j++;
-                                }
-                            }
-                            if (draws.length > 1) {
-                                reactionID =
-                                    draws[
-                                        Math.floor(
-                                            Math.random() *
-                                                Math.floor(draws.length)
-                                        )
-                                    ];
-                            }
-
-                            const squadResult = {
-                                color: 0x009900,
-                                title: `${title}`,
-                                fields: [
-                                    {
-                                        name: `${winText}`,
-                                        value: `${
-                                            reactions[reactionID]._emoji
-                                        }`,
-                                    },
-                                ],
-                                timestamp: new Date(),
-                                footer: {
-                                    icon_url: client.user.avatarURL,
-                                },
-                            };
-
-                            embedMessage.delete();
-                            games_channel.send({ embed: squadResult });
-                            if (client.config.host_channel_messages === true) {
-                                host_channel.send(
-                                    `${winText} ${reactions[reactionID]._emoji}`
-                                );
-                            }
-                        }, client.config.default_timer * 60 * 1000);
-                    });
-            }
-            catch (error) {
-                console.log(`${error}`);
-            }
+            squadTryMessage(squad_sizes);
         }
     }
 };
