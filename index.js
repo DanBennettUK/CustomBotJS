8683f8d33ae75c3c95a53d6414fca52b2a5bbfcb
Dan Bennett <dan@danbennett.me>
2019-07-18 10:56:04 +0100

    :sparkles: Added role assignment via reaction click

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const Enmap = require('enmap');
 const fs = require('fs');
 const config = require('./config.json');
 const client = new Discord.Client();
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
+const { document } = new JSDOM('').window;
 global.document = document;
 
 const $ = require('jquery')(window);
@@ -20,6 +20,7 @@ fs.readdir('./events/', (err, files) => {
     files.forEach(file => {
         const event = require(`./events/${file}`);
         const eventName = file.split('.')[0];
+        console.log(`Attempting to load event ${eventName}`);
         client.on(eventName, event.bind(null, client));
     });
 });
@@ -41,18 +42,21 @@ fs.readdir('./commands/', (err, files) => {
 client.on('ready', () => {
     console.log(`${client.user.username} is ready for action!`);
 
-    $.ajax({ 
-        dataType:'json',
-        url:`https://api.twitch.tv/helix/streams/?user_login=${config.activity.twitchUsername}`,
+    $.ajax({
+        dataType: 'json',
+        url: `https://api.twitch.tv/helix/streams/?user_login=${
+            config.activity.twitchUsername
+        }`,
         headers: {
-            'Client-ID': config.activity.twitch_client_id
-          },
-        success:function (channel) {
+            'Client-ID': config.activity.twitch_client_id,
+        },
+        success: function(channel) {
             if (channel.data.length > 0) {
                 client.user.setActivity(channel.data[0].title, {
                     url: `https://twitch.tv/${config.activity.twitchUsername}`,
-                })
-            } else {
+                });
+            }
+            else {
                 client.user.setActivity(config.activity.message, {
                     type: 'WATCHING',
                     // PLAYING, LISTENING, WATCHING
@@ -61,29 +65,34 @@ client.on('ready', () => {
                 // dnd, idle, online, invisible
             }
         },
-        error:function () {
+        error: function() {
             client.user.setActivity(config.activity.message, {
                 type: 'WATCHING',
                 // PLAYING, LISTENING, WATCHING
             });
             client.user.setStatus('dnd');
             // dnd, idle, online, invisible
-        }
+        },
     });
 
-    setInterval (function () {
-        $.ajax({ 
-            dataType:'json',
-            url:`https://api.twitch.tv/helix/streams/?user_login=${config.activity.twitchUsername}`,
+    setInterval(function() {
+        $.ajax({
+            dataType: 'json',
+            url: `https://api.twitch.tv/helix/streams/?user_login=${
+                config.activity.twitchUsername
+            }`,
             headers: {
-                'Client-ID': config.activity.twitch_client_id
-              },
-            success:function (channel) {
+                'Client-ID': config.activity.twitch_client_id,
+            },
+            success: function(channel) {
                 if (channel.data.length > 0) {
                     client.user.setActivity(channel.data[0].title, {
-                        url: `https://twitch.tv/${config.activity.twitchUsername}`,
-                    })
-                } else {
+                        url: `https://twitch.tv/${
+                            config.activity.twitchUsername
+                        }`,
+                    });
+                }
+                else {
                     client.user.setActivity(config.activity.message, {
                         type: 'WATCHING',
                         // PLAYING, LISTENING, WATCHING
@@ -92,16 +101,90 @@ client.on('ready', () => {
                     // dnd, idle, online, invisible
                 }
             },
-            error:function () {
+            error: function() {
                 client.user.setActivity(config.activity.message, {
                     type: 'WATCHING',
                     // PLAYING, LISTENING, WATCHING
                 });
                 client.user.setStatus('dnd');
                 // dnd, idle, online, invisible
-            }
+            },
         });
     }, 30000);
+});
+
+client.on('raw', async event => {
+    const eventName = event.t;
+    if (eventName === 'MESSAGE_REACTION_ADD') {
+        if (event.d.message_id === config.role_message_id) {
+            const reactionChannel = client.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id)) {
+                return;
+            }
+            else {
+                reactionChannel
+                    .fetchMessage(event.d.message_id)
+                    .then(msg => {
+                        const msgReaction = msg.reactions.get(
+                            event.d.emoji.name
+                        );
+                        const user = client.users.get(event.d.user_id);
+                        client.emit('messageReactionAdd', msgReaction, user);
+                    })
+                    .catch(err => console.log(err));
+            }
+        }
+    }
+    else if (eventName === 'MESSAGE_REACTION_REMOVE') {
+        if (event.d.message_id === config.role_message_id) {
+            const reactionChannel = client.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id)) {
+                return;
+            }
+            else {
+                reactionChannel
+                    .fetchMessage(event.d.message_id)
+                    .then(msg => {
+                        const msgReaction = msg.reactions.get(
+                            event.d.emoji.name
+                        );
+                        const user = client.users.get(event.d.user_id);
+                        client.emit('messageReactionRemove', msgReaction, user);
+                    })
+                    .catch(err => console.log(err));
+            }
+        }
+    }
+});
+client.on('messageReactionAdd', (messageReaction, user) => {
+    const roleID = config.subscriber_role_id;
+    const role = messageReaction.message.guild.roles.find(
+        role => role.id === roleID
+    );
+
+    if (role) {
+        const member = messageReaction.message.guild.members.find(
+            member => member.id === user.id
+        );
+        if (member) {
+            member.addRole(role.id);
+        }
+    }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+    const roleID = config.subscriber_role_id;
+    const role = messageReaction.message.guild.roles.find(
+        role => role.id === roleID
+    );
+
+    if (role) {
+        const member = messageReaction.message.guild.members.find(
+            member => member.id === user.id
+        );
+        if (member) {
+            member.removeRole(role.id);
+        }
+    }
 });
 
 // Debug errors
