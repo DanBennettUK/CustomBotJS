05d6474a1aaa2adb3ba194c2eb0110ee507138cc
Your Name <sergejmatek@gmail.com>
2019-07-19 13:26:10 +0200

    Made ping fancier

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,28 @@
 exports.run = async (client, message, args) => {
-    const ping_message = await message.channel.send('Ping?');
-    ping_message
-        .edit(
-            `Pong! Latency is ${ping_message.createdTimestamp -
-                message.createdTimestamp}ms. API Latency is ${Math.round(
-                client.ping
-            )}ms`
-        )
-        .catch(console.error);
+
+    let pingEmbed = {
+        color: 0x3366ff,
+        title: `Ping`,
+        timestamp: new Date(),
+        footer: {
+            icon_url: client.user.avatarURL,
+        }
+    };
+
+    const ping_message = await message.channel.send({embed: pingEmbed});
+    pingEmbed.description = `Pong!`;
+    pingEmbed.fields = [
+        {
+            name: `Latency`,
+            value: `${ping_message.createdTimestamp -
+            message.createdTimestamp}ms`,
+            inline: true
+        },
+        {
+            name: `API latency`,
+            value: `${Math.round(client.ping)}ms`,
+            inline: true
+        }
+    ];
+    ping_message.edit({embed: pingEmbed}).catch(console.error);
 };
