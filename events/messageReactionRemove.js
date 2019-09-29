477020a6f0b44afe5326d459482825455bca4ffb
Your Name <sergejmatek@gmail.com>
2019-09-29 19:15:45 +0200

    Crash hotfix

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,4 +1,5 @@
 module.exports = (client, messageReaction, user) => {
+    if (user.bot) return;
     if (messageReaction.message.id === client.config.role_message_id && messageReaction.emoji.name == client.config.role_reaction_emoji) {
         const guild = messageReaction.message.guild;
         guild.member(user.id).removeRole(guild.roles.find(r => r.id === client.config.custom_role_id)).catch(console.error);
