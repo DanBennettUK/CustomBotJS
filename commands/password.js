7e3aa09894679d760348b5eca8795b48a32332f4
Your Name <sergejmatek@gmail.com>
2020-05-18 22:00:04 +0200

    password

--- commands/password.js
+++ commands/password.js
@@ -1,24 +1,29 @@
-exports.run = async (client, message, args) => {
+const Discord = require('discord.js');
+const config = require('../config.json');
+const { client } = require('../index');
 
-    if (message.channel.id !== client.config.host_channel_id) {
+/**@param {Discord.Message} message @param {String} args*/
+module.exports = async (message, args) => {
+
+    if (message.channel.id !== config.host_channel_id) {
         // If the command isn't ran in the host channel, do nothing.
         return;
     }
 
     // Get customRole for pinging later
-    const customRole = message.guild.roles.get(client.config.custom_role_id);
+    const customRole = message.guild.roles.get(config.custom_role_id);
     const subscriberRole = message.guild.roles.get(
-        client.config.subscriber_role_id
+        config.subscriber_role_id
     );
-    const host_channel = client.channels.get(client.config.host_channel_id);
-    const games_channel = client.channels.get(client.config.games_channel_id);
+    const host_channel = client.channels.get(config.host_channel_id);
+    const games_channel = client.channels.get(config.games_channel_id);
     const subscriber_channel = client.channels.get(
-        client.config.subscriber_channel_id
+        config.subscriber_channel_id
     );
     const title = 'Custom Game Server Details';
 
     let serverPassword = args[0];
-    let timer = client.config.default_timer;
+    let timer = config.default_timer;
     let serverName = args[2];
     let raiseError = false;
     let timeLeft;
@@ -34,8 +39,8 @@ exports.run = async (client, message, args) => {
     }
 
     if (typeof serverPassword === 'undefined') {
-        if (client.config.default_game_server_password !== '') {
-            serverPassword = client.config.default_game_server_password;
+        if (config.default_game_server_password !== '') {
+            serverPassword = config.default_game_server_password;
         }
         else {
             raiseError = true;
@@ -43,8 +48,8 @@ exports.run = async (client, message, args) => {
         }
     }
     if (typeof serverName === 'undefined') {
-        if (client.config.default_game_server_name !== '') {
-            serverName = client.config.default_game_server_name;
+        if (config.default_game_server_name !== '') {
+            serverName = config.default_game_server_name;
         }
         else {
             raiseError = true;
@@ -146,7 +151,7 @@ exports.run = async (client, message, args) => {
         await customRole
             .setMentionable(false, 'Role no longer needs to be pinged')
             .catch(console.error);
-        if (client.config.host_channel_messages === true) {
+        if (config.host_channel_messages === true) {
             host_channel.send('Password has been posted!');
         }
     }
@@ -169,11 +174,11 @@ exports.run = async (client, message, args) => {
             await games_channel
                 .send({ embed: prePasswordMessage })
                 .then(async embedMessage => {
-                    const timeToVote = setTimeout(async function() {
+                    const timeToVote = setTimeout(async function () {
                         embedMessage.delete();
 
                         games_channel.send({ embed: passwordMessage });
-                        if (client.config.custom_role_ping == true) {
+                        if (config.custom_role_ping == true) {
                             await customRole
                                 .setMentionable(true, 'Role needs to be pinged')
                                 .catch(console.error);
@@ -187,12 +192,12 @@ exports.run = async (client, message, args) => {
                                 )
                                 .catch(console.error);
                         }
-                        if (client.config.host_channel_messages === true) {
+                        if (config.host_channel_messages === true) {
                             host_channel.send('Password has been posted!');
                         }
                     }, timer * 60 * 1000);
                     // Checks if message is deleted
-                    const checkIfDeleted = setInterval(function() {
+                    const checkIfDeleted = setInterval(function () {
                         if (embedMessage.deleted) {
                             clearTimeout(timeToVote);
                             clearInterval(checkIfDeleted);
