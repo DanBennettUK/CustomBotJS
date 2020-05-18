14017cc0a6f7ba0a7629c4e974e9c66ce16be8e0
Your Name <sergejmatek@gmail.com>
2020-05-18 21:54:21 +0200

    custom

--- commands/custom.js
+++ commands/custom.js
@@ -1,23 +1,30 @@
-exports.run = async (client, message, args) => {
-    if (message.channel.id !== client.config.host_channel_id) {
+const Discord = require('discord.js');
+const { client } = require('../index');
+const config = require('../config.json');
+
+/**@param {Discord.Message} message @param {String} args*/
+module.exports = async (message, args) => {
+    if (message.channel.id !== config.host_channel_id) {
         // If the command isn't ran in the host channel, do nothing.
         return;
     }
-    const customRole = message.guild.roles.get(client.config.custom_role_id);
-    const host_channel = client.channels.get(client.config.host_channel_id);
-    const games_channel = client.channels.get(client.config.games_channel_id);
+    const customRole = message.guild.roles.get(config.custom_role_id);
+    const host_channel = client.channels.get(config.host_channel_id);
+    const games_channel = client.channels.get(config.games_channel_id);
 
     await customRole.setMentionable(true, 'Role needs to be pinged').catch(console.error);
     await games_channel.send(`${customRole} ${args.join(' ')}`).catch(console.error);
     await customRole.setMentionable(false, 'Role no longer needs to be pinged').catch(console.error);
 
-    host_channel.send({embed: {
-        color: 0x3366ff,
-        title: 'Message sent',
-        description: `Message:\n${customRole} ${args.join(' ')}`,
-        timestamp: new Date(),
-        footer: {
-            icon_url: client.user.avatarURL,
+    host_channel.send({
+        embed: {
+            color: 0x3366ff,
+            title: 'Message sent',
+            description: `Message:\n${customRole} ${args.join(' ')}`,
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+            }
         }
-    }}).catch(console.error);
+    }).catch(console.error);
 }
