7a84e976c1da751542ff93e0e848d9a3285facfb
Your Name <sergejmatek@gmail.com>
2020-05-18 21:55:33 +0200

    hello

--- commands/hello.js
+++ commands/hello.js
@@ -1,16 +1,21 @@
-exports.run = async (client, message) => {
-    const customRole = message.guild.roles.get(client.config.custom_role_id);
+const Discord = require('discord.js');
+const { client } = require('../index');
+const config = require('../config.json');
 
-    if (message.channel.id === client.config.host_channel_id) {
+/**@param {Discord.Message} message @param {String} args*/
+module.exports = async (message, args) => {
+    const customRole = message.guild.roles.get(config.custom_role_id);
+
+    if (message.channel.id === config.host_channel_id) {
         message.delete();
-        const channel = client.channels.get(client.config.host_channel_id);
+        const channel = client.channels.get(config.host_channel_id);
         channel.send('Hello! :wave:');
     }
 
     // Send message to games channel
-    if (message.channel.id === client.config.games_channel_id) {
+    if (message.channel.id === config.games_channel_id) {
         message.delete();
-        const channel = client.channels.get(client.config.games_channel_id);
+        const channel = client.channels.get(config.games_channel_id);
         await customRole
             .setMentionable(true, 'Role needs to be pinged')
             .catch(console.error);
