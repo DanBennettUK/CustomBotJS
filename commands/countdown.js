4b08642d8d593dd51cfcefc2e72121cea3ca7bbf
Your Name <sergejmatek@gmail.com>
2020-05-18 21:52:58 +0200

    countdown

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,16 +1,21 @@
-exports.run = async (client, message, args) => {
+const Discord = require('discord.js');
+const { client } = require('../index');
+const config = require('../config.json');
 
-    if (message.channel.id !== client.config.host_channel_id) {
+/**@param {Discord.Message} message @param {String[]} args*/
+module.exports = async (message, args) => {
+
+    if (message.channel.id !== config.host_channel_id) {
         // If the command isn't ran in the host channel, do nothing.
         return;
     }
-    
-    const host_channel = client.channels.get(client.config.host_channel_id);
-    const games_channel = client.channels.get(client.config.games_channel_id);
+
+    const host_channel = client.channels.get(config.host_channel_id);
+    const games_channel = client.channels.get(config.games_channel_id);
     const countdownText = 'The countdown will end in';
     const countdownEndText = 'Countdown Ended!';
     let timerText;
-    let timer = client.config.default_timer;
+    let timer = config.default_timer;
 
     if (args.length > 0) {
         if (parseInt(args[args.length - 1]) || args[args.length - 1] == 0) {
@@ -53,7 +58,7 @@ exports.run = async (client, message, args) => {
         await games_channel
             .send({ embed: countdownEmbed })
             .then(async embedMessage => {
-                const countdownInterval = setTimeout(function() {
+                const countdownInterval = setTimeout(function () {
                     const countdownEndedEmbed = {
                         color: 0x009900,
                         title: `${countdownEndText}`,
@@ -64,18 +69,18 @@ exports.run = async (client, message, args) => {
                     };
                     embedMessage.delete();
                     games_channel.send({ embed: countdownEndedEmbed });
-                    if (client.config.host_channel_messages === true) {
+                    if (config.host_channel_messages === true) {
                         host_channel.send({ embed: countdownEndedEmbed });
                     }
                 }, timer * 60 * 1000);
-            // Checks if message is deleted
-            const checkIfDeleted = setInterval(function() {
-                if (embedMessage.deleted) {
-                    clearTimeout(countdownInterval);
-                    clearInterval(checkIfDeleted);
-                }
-            }, 1000);
-        });
+                // Checks if message is deleted
+                const checkIfDeleted = setInterval(function () {
+                    if (embedMessage.deleted) {
+                        clearTimeout(countdownInterval);
+                        clearInterval(checkIfDeleted);
+                    }
+                }, 1000);
+            });
     }
     catch (error) {
         console.log(`${error}`);
