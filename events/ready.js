3473e78ce6ec3be41e5f7a00cbfe090d634ab049
Your Name <sergejmatek@gmail.com>
2020-05-18 21:50:03 +0200

    ready

--- events/ready.js
+++ events/ready.js
@@ -1,41 +1,45 @@
-module.exports = (client) => {
+const config = require('../config.json');
+const request = require('request');
+const { client } = require('../index');
+
+module.exports = () => {
     // On connect do these:
-    const request = client.request;
     setTimeout(() => {
         console.log(`${client.user.username} is ready for action!`);
-        const roleChannel = client.channels.get(client.config.role_channel_id);
-        if (client.config.role_message_id !== "") {
-            roleChannel.fetchMessage(client.config.role_message_id).then(msg => msg.react(client.config.role_reaction_emoji)).catch(console.error);
+        const roleChannel = client.channels.get(config.role_channel_id);
+        if (config.role_message_id !== "") {
+            roleChannel.fetchMessage(config.role_message_id).then(msg => msg.react(config.role_reaction_emoji)).catch(console.error);
         }
 
+        //Won't work
         function checkTwitch() {
             request({
-                url: `https://api.twitch.tv/helix/streams/?user_login=${client.config.activity.twitchUsername}`,
+                url: `https://api.twitch.tv/helix/streams/?user_login=${config.activity.twitchUsername}`,
                 headers: {
-                    'Client-ID': client.config.activity.twitch_client_id
+                    'Client-ID': config.activity.twitch_client_id
                 }
             }, (error, response, body) => {
                 if (!error) {
                     const channel = JSON.parse(body);
                     if (channel.data.length > 0) {
                         client.user.setActivity(channel.data[0].title, {
-                            url: `https://twitch.tv/${client.config.activity.twitchUsername}`,
+                            url: `https://twitch.tv/${config.activity.twitchUsername}`,
                         });
                     } else {
-                        client.user.setActivity(client.config.activity.message, {
+                        client.user.setActivity(config.activity.message, {
                             type: 'WATCHING',
                             // PLAYING, LISTENING, WATCHING
                         });
-                        client.user.setStatus(client.config.activity.status);
+                        client.user.setStatus(config.activity.status);
                         // dnd, idle, online, invisible
                     }
                 } else {
                     console.log(error);
-                    client.user.setActivity(client.config.activity.message, {
+                    client.user.setActivity(config.activity.message, {
                         type: 'WATCHING',
                         // PLAYING, LISTENING, WATCHING
                     });
-                    client.user.setStatus(client.config.activity.status);
+                    client.user.setStatus(config.activity.status);
                     // dnd, idle, online, invisible
                 }
             });
