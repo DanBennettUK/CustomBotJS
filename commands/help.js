cddeaf7e69d2179e63f205e64665f8c70d2f82c1
Your Name <sergejmatek@gmail.com>
2020-06-23 19:19:34 +0200

    change help.js

--- commands/help.js
+++ commands/help.js
@@ -10,145 +10,81 @@ module.exports = async (message, args) => {
         return;
     }
 
-    const configPrefix = config.prefix;
-
-    if (
-        !message.member.roles.cache.some(
-            role => role.id === config.host_role_id
-        )
-    ) {
+    if (!message.member.roles.cache.some(role => role.id === config.host_role_id)) {
         try {
-            message.channel.send({
-                embed: {
-                    color: 0x666633,
-                    author: {
-                        name: client.user.username,
-                        icon_url: client.user.displayAvatarURL(),
-                        url: 'https://github.com/DanBennettUK/CustomBotJS',
-                    },
-                    title: 'Commands for users',
-                    fields: [
-                        {
-                            name: `${configPrefix}help`,
-                            value: 'lists all commands',
-                        },
-                        {
-                            name: `${configPrefix}ping`,
-                            value: 'checks Discord API response',
-                        },
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        icon_url: client.user.displayAvatarURL(),
-                        text: '© DanBennett',
-                    },
-                },
-            });
+            message.channel.send(new Discord.MessageEmbed()
+                .setColor(0x666633)
+                .setAuthor(client.user.username, client.user.displayAvatarURL(),
+                    'https://github.com/DanBennettUK/CustomBotJS')
+                .setTitle('Commands for users')
+                .addField(`${config.prefix}help`, 'lists all commands')
+                .addField(`${config.prefix}ping`, 'checks Discord API response')
+                .setTimestamp()
+                .setFooter('', client.user.displayAvatarURL())).catch(console.error)
         }
         catch (error) {
             console.log(`${error}`);
         }
     }
-    else if (
-        message.member.roles.cache.some(
-            role => role.id === config.host_role_id
-        )
-    ) {
+    else {
         try {
-            message.channel.send({
-                embed: {
-                    color: 0x666633,
-                    author: {
-                        name: client.user.username,
-                        icon_url: client.user.displayAvatarURL(),
-                        url: 'https://github.com/DanBennettUK/CustomBotJS',
-                    },
-                    title: 'Commands for hosts',
-                    fields: [
-                        {
-                            name: `\`${configPrefix}help\``,
-                            value: 'lists all commands',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}ping\``,
-                            value: 'checks Discord API response',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}squadvote (sqv) <sizes> [-minutes]\``,
-                            value:
-                                'Starts squadvote. \nUsage: `all` for 1-10 \nLeave blank for 1 2 4 8 \nChoose your own (e.g. `squadvote 3 4 6 8`). For an optional custom timer, use `-[minutes]` at the end.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}perspectivevote (pv) [minutes]\``,
-                            value:
-                                'Starts perspective vote. [minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}regionvote (rv) [minutes]\``,
-                            value: 'Starts region vote. [minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}mapvote (mv) [warmode (wm)] <maps> [minutes]\``,
-                            value: 'Starts map vote. \nUsage: leave blank or `all` for all maps \nChoose your own maps (e.g. `mapvote erangel miramar`) \nuse [warmode] to add Camp Jackal to the map pool \n[minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}weathervote (wv) [map] [minutes]\``,
-                            value: 'Starts weather vote. \nUsage: leave blank or `all` for all weather \nEnter a map for all weather available to the map \nChoose your own (e.g. `weathervote sunny rainy foggy`) \n[minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}warmodeweaponsvote (wmwv) [minutes]\``,
-                            value:
-                                'Starts warmode weapons vote. [minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}warmodegametypevote (wmgv) [minutes]\``,
-                            value:
-                                'Starts warmode game type vote. [minutes] is optional.',
-                            inline: false
-                        },
-                        {
-                            name: `\`${configPrefix}startgame [minutes]\``,
-                            value:
-                                'Starts a countdown for the game to start. [minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}countdown [minutes]\``,
-                            value: 'Starts a countdown. [minutes] is optional.',
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}vclimit <limit>\``,
-                            value: `Sets voice channels starting with ${
-                                config.voice_channel_emoji
-                                } to <limit>`,
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}clear\``,
-                            value: `Clears bot messages from games channel \nUsage: \`all\` to remove all messages \nNumber of messages you want to delete \nExample: \`${configPrefix}clear 10\``,
-                            inline: false,
-                        },
-                        {
-                            name: `\`${configPrefix}custom [message]\``,
-                            value: `Pings the custom role and sends the provided message. \nExample: \`${configPrefix}custom get voting!\`\nResult: \`@Custom get voting!\` is posted in ${client.channels.get(config.games_channel_id)}`
-                        }
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        icon_url: client.user.displayAvatarURL(),
-                        text: `© DanBennett - Version: ${client.version}`,
-                    },
-                },
-            });
+            message.channel.send(new Discord.MessageEmbed()
+                .setColor(0x666633)
+                .setAuthor(client.user.username, client.user.displayAvatarURL(),
+                    'https://github.com/DanBennettUK/CustomBotJS')
+                .setTitle('Commands for hosts')
+                .addField(`${config.prefix}help`, 'lists all commands')
+                .addField(`${config.prefix}ping`, 'checks Discord API response')
+                .addField(`${config.prefix}squadvote (sqv) <sizes> [-minutes]`,
+                    `Starts squadvote.
+                    Usage:
+                        \`all\` for 1-10
+                        Leave blank for 1 2 4 8
+                        Choose your own (e.g. \`squadvote 3 4 6 8\`)
+                    For an optional custom timer, use \`-[minutes]\` at the end.`)
+                .addField(`${config.prefix}perspectivevote (pv) [minutes]`,
+                    'Starts perspective vote. [minutes] is optional')
+                .addField(`${config.prefix}regionvote (rv) [minutes]`,
+                    'Starts region vote. [minutes] is optional')
+                .addField(`${config.prefix}mapvote (mv) [warmode (wm)] <maps> [minutes]`,
+                    `Starts map vote.
+                Usage:
+                    Leave blank or \`all\` for all maps
+                    Choose your own maps (e.g. \`mapvote erangel miramar\`)
+                    use [warmode] to add Camp Jackal to the map pool
+                [minutes] is optional.`)
+                .addField(`${config.prefix}weathervote (wv) [map] [minutes]`,
+                    `Starts weather vote.
+                Usage:
+                    Leave blank or \`all\` for all weather
+                    Enter a map for all weather available to the map
+                    Choose your own (e.g. \`weathervote sunny rainy foggy\`)
+                [minutes] is optional.`)
+                .addField(`${config.prefix}warmodeweaponsvote (wmwv) [minutes]`,
+                    'Starts warmode weapons vote. [minutes] is optional')
+                .addField(`${config.prefix}warmodegametypevote (wmgv) [minutes]`,
+                    'Starts warmode weapons vote. [minutes] is optional.')
+                .addField(`${config.prefix}startgame [minutes]`,
+                    'Starts a countdown for the game to start. [minutes] is optional.')
+                .addField(`${config.prefix}countdown [minutes]`,
+                    'Starts a countdown. [minutes] is optional')
+                .addField(`${config.prefix}vclimit <limit>`,
+                    `Sets voice channels starting with ${config.voice_channel_emoji} to <limit>`)
+                .addField(`${config.prefix}clear`,
+                    `Clears bot messages from games channel
+                    Usage:
+                        \`all\` to remove all messages
+                        Number of messages you want to delete
+                    Example: \`${config.prefix}clear 10\``)
+                .addField(`${config.prefix}custom [message]`,
+                    `Clears bot messages from games channel
+                    Usage:
+                        \`all\` to remove all messages
+                        Number of messages you want to delete
+                    Example: \`${config.prefix}clear 10\``)
+                .setTimestamp()
+                .setFooter('', client.user.displayAvatarURL())
+            ).catch(console.error);
         }
         catch (error) {
             console.log(`${error}`);
