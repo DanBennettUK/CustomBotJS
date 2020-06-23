2b6a53a3eb64c10044d5f4441773252e1bd7fb7d
Your Name <sergejmatek@gmail.com>
2020-06-23 20:18:55 +0200

    change mapvote.js

--- commands/mapvote.js
+++ commands/mapvote.js
@@ -19,12 +19,9 @@ module.exports = async (message, args) => {
     /**@type {Discord.TextChannel} */
     const games_channel = client.channels.cache.get(config.games_channel_id);
     let timer = config.default_timer;
-    let timerText;
+    let timerText = '';
 
     // Set up the message as an embed, ready to post
-    const title = 'Vote for map!';
-    const description = 'Please vote on the map for the next game!';
-    const winValue = 'The winning map is:';
     let mapChoices = [];
 
     args.forEach(function (arg, i) {
@@ -37,44 +34,35 @@ module.exports = async (message, args) => {
             args.splice(args.length - 1, 1);
         }
         if (isNaN(timer)) {
-            const error = {
-                color: 0xff0000,
-                title: 'Error!',
-                description: 'Minutes is missing or not a number!',
-                timestamp: new Date(),
-                footer: {
-                    icon_url: client.user.displayAvatarURL()
-                }
-            };
-            host_channel.send({ embed: error });
+            host_channel.send(new Discord.MessageEmbed()
+                .setColor(0xff0000)
+                .setTitle('Error!')
+                .setDescription('Minutes is missing or not a number!')
+                .setTimestamp()
+                .setFooter('', client.user.displayAvatarURL())
+            ).catch(console.error);
             return;
         }
         if (args[0] === 'wm') args[0] = 'warmode';
     }
 
-    if (timer == 1) {
+    if (timer == 1)
         timerText = 'minute';
-    }
-    else {
+    else
         timerText = 'minutes';
-    }
     if (args.length > 0) {
         if (args[0] !== 'all' && args[0] !== 'warmode') {
-            if (args.some(map => map.includes('erangel'))) {
+            if (args.some(map => map.includes('erangel')))
                 mapChoices.push(`${emojiCharacters['Erangel']} for Erangel`);
-            }
-            if (args.some(map => map.includes('miramar'))) {
+            if (args.some(map => map.includes('miramar')))
                 mapChoices.push(`${emojiCharacters['Miramar']} for Miramar`);
-            }
-            if (args.some(map => map.includes('sanhok'))) {
+            if (args.some(map => map.includes('sanhok')))
                 mapChoices.push(`${emojiCharacters['Sanhok']} for Sanhok`);
-            }
-            if (args.some(map => map.includes('vikendi'))) {
+            if (args.some(map => map.includes('vikendi')))
                 mapChoices.push(`${emojiCharacters['Vikendi']} for Vikendi`);
-            }
         }
         else if (args[0] === 'warmode') {
-            if (!args.some(map => ['erangel', 'miramar', 'sanhok', 'vikendi'].includes(map))) {
+            if (!args.some(map => ['erangel', 'miramar', 'sanhok', 'vikendi'].includes(map)))
                 mapChoices = [
                     `${emojiCharacters['Erangel']} for Erangel`,
                     `${emojiCharacters['Miramar']} for Miramar`,
@@ -82,111 +70,83 @@ module.exports = async (message, args) => {
                     `${emojiCharacters['Vikendi']} for Vikendi`,
                     `${emojiCharacters['Jackal']} for Camp Jackal`
                 ];
-            } else {
-                if (args.some(map => map.includes('erangel'))) {
+            else {
+                if (args.some(map => map.includes('erangel')))
                     mapChoices.push(`${emojiCharacters['Erangel']} for Erangel`);
-                }
-                if (args.some(map => map.includes('miramar'))) {
+                if (args.some(map => map.includes('miramar')))
                     mapChoices.push(`${emojiCharacters['Miramar']} for Miramar`);
-                }
-                if (args.some(map => map.includes('sanhok'))) {
+                if (args.some(map => map.includes('sanhok')))
                     mapChoices.push(`${emojiCharacters['Sanhok']} for Sanhok`);
-                }
-                if (args.some(map => map.includes('vikendi'))) {
+                if (args.some(map => map.includes('vikendi')))
                     mapChoices.push(`${emojiCharacters['Vikendi']} for Vikendi`);
-                }
-                if (args.some(map => map.includes('jackal'))) {
+                if (args.some(map => map.includes('jackal')))
                     mapChoices.push(`${emojiCharacters['Jackal']} for Camp Jackal`);
-                }
             }
         }
-        else {
+        else
             mapChoices = [
                 `${emojiCharacters['Erangel']} for Erangel`,
                 `${emojiCharacters['Miramar']} for Miramar`,
                 `${emojiCharacters['Sanhok']} for Sanhok`,
                 `${emojiCharacters['Vikendi']} for Vikendi`,
             ];
-        }
     }
-    else {
+    else
         mapChoices = [
             `${emojiCharacters['Erangel']} for Erangel`,
             `${emojiCharacters['Miramar']} for Miramar`,
             `${emojiCharacters['Sanhok']} for Sanhok`,
             `${emojiCharacters['Vikendi']} for Vikendi`,
         ];
-    }
-    const choices = mapChoices.join('\n');
 
-    const mapVoteMessage = {
-        color: 0x3366ff,
-        title: `${title}`,
-        description: `${description}`,
-        fields: [
-            {
-                name: 'Choose a reaction',
-                value: choices,
-            },
-            {
-                name: 'Vote will close in:',
-                value: `${timer} ${timerText}`,
-            },
-        ],
-        timestamp: new Date(),
-        footer: {
-            icon_url: client.user.displayAvatarURL(),
-        }
-    };
     if (timer == 0) {
-        const randomMapEmbed = {
-            color: 0x3366ff,
-            title: `Random map selection`,
-            description: `The map for the next game will be chosen randomly out of the choices provided`,
-            fields: [
-                {
-                    name: 'Choices',
-                    value: choices,
-                    inline: true
-                },
-                {
-                    name: 'Selection',
-                    value: mapChoices[Math.floor(Math.random() * Math.floor(mapChoices.length))],
-                    inline: true
-                }
-            ],
-            timestamp: new Date(),
-            footer: {
-                icon_url: client.user.displayAvatarURL(),
-            }
-        };
-        games_channel.send({ embed: randomMapEmbed }).catch(console.error);
+        games_channel.send(new Discord.MessageEmbed()
+            .setColor(0x3366ff)
+            .setTitle('Random map selection')
+            .setDescription('The map for the next game will be chosen randomly out of the choices provided')
+            .addField('Choices', mapChoices.join('\n'), true)
+            .addField('Selection', mapChoices[Math.floor(Math.random() * Math.floor(mapChoices.length()))], true)
+            .setTimestamp()
+            .setFooter('', client.user.displayAvatarURL())
+        ).catch(console.error);
         if (config.host_channel_messages === true) {
-            host_channel.send({ embed: randomMapEmbed }).catch(console.error);
+            host_channel.send(new Discord.MessageEmbed()
+                .setColor(0x3366ff)
+                .setTitle('Random map selection')
+                .setDescription('The map for the next game will be chosen randomly out of the choices provided')
+                .addField('Choices', mapChoices.join('\n'), true)
+                .addField('Selection', mapChoices[Math.floor(Math.random() * Math.floor(mapChoices.length()))], true)
+                .setTimestamp()
+                .setFooter('', client.user.displayAvatarURL())
+            ).catch(console.error);
         }
     }
     else {
         try {
             await games_channel
-                .send({ embed: mapVoteMessage })
+                .send(new Discord.MessageEmbed()
+                    .setColor(0x3366ff)
+                    .setTitle('Vote for map!')
+                    .setDescription('Please vote on the map for the next game!')
+                    .addField('Choose a reaction', mapChoices.join('\n'))
+                    .addField('Vote will close in', `${timer} ${timerText}`)
+                    .setTimestamp()
+                    .setFooter('', client.user.displayAvatarURL())
+                )
                 .then(async embedMessage => {
                     /**@param {Discord.MessageReaction} reaction @param {Discord.User} user*/
                     const filter = (reaction, user) => reaction.users.cache.has(client.user.id);
                     const collector = embedMessage.createReactionCollector(filter);
                     if (args.length > 0) {
                         if (args[0] !== 'all' && args[0] !== 'warmode') {
-                            if (args.some(map => map.includes('erangel'))) {
+                            if (args.some(map => map.includes('erangel')))
                                 await embedMessage.react(emojiCharacters['Erangel']);
-                            }
-                            if (args.some(map => map.includes('miramar'))) {
+                            if (args.some(map => map.includes('miramar')))
                                 await embedMessage.react(emojiCharacters['Miramar']);
-                            }
-                            if (args.some(map => map.includes('sanhok'))) {
+                            if (args.some(map => map.includes('sanhok')))
                                 await embedMessage.react(emojiCharacters['Sanhok']);
-                            }
-                            if (args.some(map => map.includes('vikendi'))) {
+                            if (args.some(map => map.includes('vikendi')))
                                 await embedMessage.react(emojiCharacters['Vikendi']);
-                            }
                         }
                         else if (args[0] === 'warmode') {
                             if (!args.some(map => ['erangel', 'miramar', 'sanhok', 'vikendi'].includes(map))) {
@@ -196,21 +156,16 @@ module.exports = async (message, args) => {
                                 await embedMessage.react(emojiCharacters['Vikendi']);
                                 await embedMessage.react(emojiCharacters['Jackal']);
                             } else {
-                                if (args.some(map => map.includes('erangel'))) {
+                                if (args.some(map => map.includes('erangel')))
                                     await embedMessage.react(emojiCharacters['Erangel']);
-                                }
-                                if (args.some(map => map.includes('miramar'))) {
+                                if (args.some(map => map.includes('miramar')))
                                     await embedMessage.react(emojiCharacters['Miramar']);
-                                }
-                                if (args.some(map => map.includes('sanhok'))) {
+                                if (args.some(map => map.includes('sanhok')))
                                     await embedMessage.react(emojiCharacters['Sanhok']);
-                                }
-                                if (args.some(map => map.includes('vikendi'))) {
+                                if (args.some(map => map.includes('vikendi')))
                                     await embedMessage.react(emojiCharacters['Vikendi']);
-                                }
-                                if (args.some(map => map.includes('jackal'))) {
+                                if (args.some(map => map.includes('jackal')))
                                     await embedMessage.react(emojiCharacters['Jackal']);
-                                }
                             }
                         }
                         else {
@@ -227,23 +182,13 @@ module.exports = async (message, args) => {
                         await embedMessage.react(emojiCharacters['Vikendi']);
                     }
                     if (config.custom_role_ping == true) {
-                        await customRole
-                            .setMentionable(true, 'Role needs to be pinged')
-                            .catch(console.error);
-                        await games_channel
-                            .send(customRole + ' - get voting!')
-                            .then(msg =>
-                                setTimeout(function () {
-                                    msg.delete();
-                                }, timer * 60 * 1000)
-                            )
-                            .catch(console.error);
-                        await customRole
-                            .setMentionable(
-                                false,
-                                'Role no longer needs to be pinged'
-                            )
-                            .catch(console.error);
+                        await customRole.setMentionable(true, 'Role needs to be pinged').catch(console.error);
+                        await games_channel.send(customRole + ' - get voting!').then(msg =>
+                            setTimeout(function () {
+                                msg.delete();
+                            }, timer * 60 * 1000)
+                        ).catch(console.error);
+                        await customRole.setMentionable(false, 'Role no longer needs to be pinged').catch(console.error);
                     }
                     collector.on('end', reactions => {
                         let reactionID;
@@ -256,18 +201,11 @@ module.exports = async (message, args) => {
                         });
                         let draws = [];
                         reactions.forEach(r => {
-                            if (r.count == maxCount) {
+                            if (r.count == maxCount)
                                 draws.push(r.emoji.name);
-                            }
                         });
-                        if (draws.length > 1) {
-                            reactionID =
-                                draws[
-                                Math.floor(
-                                    Math.random() * Math.floor(draws.length)
-                                )
-                                ];
-                        }
+                        if (draws.length > 1)
+                            reactionID = draws[Math.floor(Math.random() * Math.floor(draws.length))];
                         let winReact;
 
                         switch (reactionID) {
@@ -286,49 +224,45 @@ module.exports = async (message, args) => {
                             case emojiCharacters['Jackal']:
                                 winReact = `${reactionID} for Camp Jackal`;
                         }
-                        let mapResult;
-                        if (draws.length > 1) {
-                            mapResult = {
-                                color: 0x009900,
-                                title: `${title}`,
-                                fields: [
-                                    {
-                                        name: 'Draws',
-                                        value: `${draws.join(' ')}`,
-                                        inline: true
-                                    },
-                                    {
-                                        name: `${winValue}`,
-                                        value: `${winReact}`,
-                                        inline: true
-                                    }
-                                ],
-                                timestamp: new Date(),
-                                footer: {
-                                    icon_url: client.user.displayAvatarURL(),
+                        let fields = [];
+                        if (draws.length > 1)
+                            fields = [
+                                {
+                                    name: 'Draws',
+                                    value: `${draws.join(' ')}`,
+                                    inline: true
+                                },
+                                {
+                                    name: 'The winning map is:',
+                                    value: `${winReact}`,
+                                    inline: true
                                 }
-                            };
-                        } else {
-                            mapResult = {
-                                color: 0x009900,
-                                title: `${title}`,
-                                fields: [
-                                    {
-                                        name: `${winValue}`,
-                                        value: `${winReact}`,
-                                    }
-                                ],
-                                timestamp: new Date(),
-                                footer: {
-                                    icon_url: client.user.displayAvatarURL(),
+                            ];
+                        else
+                            fields = [
+                                {
+                                    name: 'The winning map is:',
+                                    value: `${winReact}`,
+                                    inline: true
                                 }
-                            };
-                        }
+                            ];
 
                         embedMessage.delete();
-                        games_channel.send({ embed: mapResult });
+                        games_channel.send(new Discord.MessageEmbed()
+                            .setColor(0x009900)
+                            .setTitle('Vote for map!')
+                            .addFields(fields)
+                            .setTimestamp()
+                            .setFooter('', client.user.displayAvatarURL())
+                        ).catch(console.error);
                         if (config.host_channel_messages === true) {
-                            host_channel.send({ embed: mapResult });
+                            host_channel.send(new Discord.MessageEmbed()
+                                .setColor(0x009900)
+                                .setTitle('Vote for map!')
+                                .addFields(fields)
+                                .setTimestamp()
+                                .setFooter('', client.user.displayAvatarURL())
+                            ).catch(console.error);
                         }
                     });
                     const timeToVote = setTimeout(function () {
