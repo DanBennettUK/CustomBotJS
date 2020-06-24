8bc4e362cd9c98b435ab4e5201998978886250f7
Your Name <sergejmatek@gmail.com>
2020-06-24 17:32:09 +0200

    change vclimit.js

--- commands/vclimit.js
+++ commands/vclimit.js
@@ -11,14 +11,11 @@ module.exports = async (message, args) => {
     }
     /**@type {Discord.TextChannel} */
     const host_channel = client.channels.cache.get(config.host_channel_id);
-    let error;
 
     if (args[0] > -1 && args[0] < 100) {
         client.channels.cache.forEach(channel => {
             if (channel.type == 'voice') {
-                if (
-                    channel.name.startsWith(config.voice_channel_emoji)
-                ) {
+                if (channel.name.startsWith(config.voice_channel_emoji)) {
                     try {
                         channel.setUserLimit(args[0]).catch(console.error);
                     }
@@ -28,31 +25,16 @@ module.exports = async (message, args) => {
                 }
             }
         });
-        host_channel.send(`:white_check_mark: Voice limit set to ${args[0]}`);
-    }
-    else if (isNaN(args[0])) {
-        error = true;
+        host_channel.send(`:white_check_mark: Voice limit set to ${args[0]}`).catch(console.error);
     }
     else {
-        error = true;
+        host_channel.send(new Discord.MessageEmbed()
+            .setColor(0xff0000)
+            .setTitle('Error!')
+            .addField('One or more of your arguments are wrong',
+                'Number must be a number and between 1 and 99, or 0 to remove the limit.')
+            .setTimestamp()
+            .setFooter('', client.user.displayAvatarURL())
+        ).catch(console.error);
     }
-    if (error === true) {
-        const errorMessage = {
-            color: 0xff0000,
-            title: 'Error!',
-            fields: [
-                {
-                    name: 'One or more of your arguments are wrong',
-                    value:
-                        'Number must be a number and between 1 and 99, or 0 to remove the limit.',
-                },
-            ],
-            timestamp: new Date(),
-            footer: {
-                icon_url: client.user.displayAvatarURL(),
-            },
-        };
-        host_channel.send({ embed: errorMessage });
-        return;
-    }
-};
+}
